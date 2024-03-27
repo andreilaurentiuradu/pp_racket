3386b2a099a009d07df6d98db667be6bf6cc00e0
Andrei-Laurențiu Radu <117320481+andreilaurentiuradu@users.noreply.github.com>
2024-03-27 03:31:55 +0200

    Add files via upload

---
+++ etapa3/etapa3.rkt
@@ -0,0 +1,110 @@
+#lang racket
+(require "suffix-tree.rkt")
+(require "etapa1.rkt")
+(require "etapa2.rkt")
+
+(provide (all-defined-out))
+
+;; Această etapă este dedicată aplicațiilor 
+;; arborelui de sufixe:
+;; - găsirea unui șablon într-un text
+;; - cel mai lung subșir comun a două texte
+;; - găsirea unui subșir de lungime dată care se
+;;   repetă în text
+;; Conform convenției din etapele anterioare, un text
+;; este întotdeauna reprezentat ca listă de caractere.
+;; Rezultatele funcțiilor de mai jos sunt de asemenea
+;; reprezentate ca liste de caractere.
+
+
+; TODO 1
+; Implementați funcția substring? care primește un text și
+; un șablon nevid și întoarce true dacă șablonul apare în 
+; text, respectiv false în caz contrar.
+; Observație: ați implementat deja logica principală a
+; acestei căutări în etapa 1, în funcția st-has-pattern?,
+; care este un operator al tipului ST. Acum aveți toate
+; uneltele necesare implementării operatorului corespunzător
+; pentru tipul text (pentru că în etapa 2 ați implementat
+; construcția arborelui de sufixe asociat unui text).
+(define (substring? text pattern)
+  (let * (
+          (ast (text->ast text))
+         )
+         (st-has-pattern?  ast pattern)
+  )
+ )
+
+; TODO 2
+; Implementați funcția longest-common-substring care primește
+; două texte și determină cel mai lung subșir comun al
+; acestora, folosind algoritmul următor:
+; 1. Construiește arborele de sufixe ST1 pentru primul text.
+; 2. Pentru fiecare sufix din al doilea text (de la cel mai
+;    lung la cel mai scurt), găsește cea mai lungă potrivire 
+;    cu sufixele din primul text, urmând căile relevante în ST1.
+; 3. Rezultatul final este cel mai lung rezultat identificat
+;    la pasul 2 (în caz de egalitate a lungimii, păstrăm
+;    primul șir găsit).
+; Folosiți named let pentru a parcurge sufixele.
+; Observație: pentru sufixele din al doilea text nu dorim 
+; marcajul de final $ pentru a nu crește artificial lungimea 
+; șirului comun cu acest caracter.
+; Hint: Revizitați funcția match-pattern-with-label (etapa 1).
+(define (lcs-helper text1 text2 ans)
+    (let traverse-suffixes (
+                            (st1 (text->cst text1)) ; arborele de sufixe pentru primul text
+                            (suffixes (get-suffixes text2)) ; sufixele pentru textul2
+                            )
+      (let* (
+             (current-common-string (foldl (lambda(x acc)   ; facem o lista care va contine cel mai bun sufix al text2 care apare in text1           
+                     (if(and
+                         (not (null? x)) ; nu e null sufixul
+                         (st-has-pattern? st1 x) ; apare in arborele de sufixe al text1
+                         (> (length x) (length acc)) ; are lungimea mai mare decat cea de pana acum
+                         )
+                        x
+                        acc
+                        )
+                     )
+            
+                   '()
+                   suffixes
+                   )
+               )
+            )
+            (if (null? text2) ; apelam recursiv pentru toate prefixele lui text2
+                  ans
+                  (if (< (length current-common-string) (length ans)) ; daca am gasit unul cu lungimea mai buna
+                      (lcs-helper text1 (drop-right text2 1) ans)
+                      (lcs-helper text1 (drop-right text2 1) current-common-string)
+                   )        
+             )
+       )
+
+      
+    )     
+ )
+(define (longest-common-substring text1 text2)  
+   (lcs-helper text1 text2 '()) ; folosim recursivitate pe stiva cu un acumulator in care retinem raspunsul de pana atunci
+ )
+
+
+; TODO 3
+; Implementați funcția repeated-substring-of-given-length
+; care primește un text și un număr natural len și
+; parcurge arborele de sufixe al textului până găsește un
+; subșir de lungime len care se repetă în text.
+; Dacă acest subșir nu există, funcția întoarce false.
+; Obs: din felul în care este construit arborele de sufixe
+; (pe baza alfabetului sortat), rezultatul va fi primul 
+; asemenea subșir din punct de vedere alfabetic.
+; Ideea este următoarea: orice cale în arborele de sufixe
+; compact care se termină cu un nod intern (un nod care 
+; are copii, nu este o frunză) reprezintă un subșir care
+; se repetă, pentru că orice asemenea cale reprezintă un
+; prefix comun pentru două sau mai multe sufixe ale textului.
+; Folosiți interfața definită în fișierul suffix-tree
+; atunci când manipulați arborele.
+(define (repeated-substring-of-given-length text len)
+  'your-code-here)
